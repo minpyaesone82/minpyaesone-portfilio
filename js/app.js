34096cd274f325d1e72c5334dcdf11c162d53cce
unknown <minpyaesone82>
2021-08-26 18:16:48 +0630

    first commit

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 
-new TypeIt(".type", {
+new TypeIt("#type", {
     speed :200,
     loop: true,
     waitUntilVisile :true,
@@ -12,13 +12,14 @@ new TypeIt(".type", {
 .go();
 
 // gsap)
-gsap.from(".navbar-brand", {opacity:1,duration: 1,delay:0.5, y: -10});
+gsap.from(".navbar-brand", {opacity:1,duration: 1,delay:0.5, y: -70});
 gsap.from(".navbar-toggler", {opacity:2,duration: 1,delay:1, x: -20});
-gsap.from(".about-img", {opacity:0,duration: 1,delay:1.5, x: -200});
-gsap.from(".content-home h3", {opacity:0,duration: 1,delay:2, y: -50});
+gsap.from(".home-img", {opacity:0,duration: 1,delay:1.5, x: -200});
+gsap.from(".content-home h6", {opacity:0,duration: 1,delay:2, y: -50});
 gsap.from(".content-home h1", {opacity:0,duration: 1,delay:2.5, y: -40});
-gsap.from(".content-home h4", {opacity:0,duration: 1,delay:3, y: -30});
-gsap.from(".nav-item", {opacity:0,duration: 1,delay:1.2, y: 50,stagger:0.2});
+gsap.from(".btn-home", {opacity:0,duration: 1,delay:3, y: -30});
+gsap.from(".icons", {opacity:0,duration: 1,delay:3.2, x: -20});
+gsap.from(".nav-item", {opacity:0,duration: 1,delay:1.2, y: 20,stagger:0.2});
 
 
 AOS.init({
